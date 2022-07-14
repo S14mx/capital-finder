30870d5699ac85999aee963b56d9961a9c22615c
S14mx <sergey.otryshko@gmail.com>
2022-07-13 19:32:18 -0700

    implemented get method to search by country/capital

---
+++ README.md
@@ -0,0 +1,9 @@
+## Project Name: Capital Finder
+
+## API
+
+Lookup by capital:
+<https://capital-finder-ruddy.vercel.app/api/capitals?capital=kyiv>
+
+Lookup by country:
+<https://capital-finder-ruddy.vercel.app/api/capitals?country=peru>
--- api/capitals.py
+++ api/capitals.py
@@ -2,12 +2,32 @@ from http.server import BaseHTTPRequestHandler
 from urllib import parse
 import requests
 
+
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
+        s = self.path
+        url_components = parse.urlsplit(s)
+        query_string_list = parse.parse_qsl(url_components.query)
+        dic = dict(query_string_list)
+        url = "https://restcountries.com/v2/"
+
+        if "country" in dic:
+            r = requests.get(url + "name/" + dic["country"])
+            data = r.json()
+            message = "The capital of " + \
+                data[0]["name"] + " is " + data[0]["capital"]
+
+        if "capital" in dic:
+            r = requests.get(url + "capital/" + dic["capital"])
+            data = r.json()
+            message = data[0]["capital"] + \
+                " is the capital of " + data[0]["name"]
+
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
-        message = "Hello World!.  We made some changes."
+
         self.wfile.write(message.encode())
+
         return
